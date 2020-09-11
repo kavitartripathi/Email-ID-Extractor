d17348cdad0efff836c4553ae3f1d0e81b370ed3
Koustubh Moharir <koustubhmoharir@gmail.com>
2020-09-11 21:27:59 +0530

    Changes based on review See README.md for the updated design

--- Program.cs
+++ Program.cs
@@ -12,273 +12,215 @@ using MailKit.Security;
 using System.Threading;
 using System.IO;
 using MailKit.Search;
+using MimeKit;
+using System.Text.RegularExpressions;
 
 namespace EmailIDsExtractor
 {
-    public class EmailAddress
-    {
-        public string Name { get; set; }
-        public string Address { get; set; }
-    }
     public class EmailConfiguration
     {
         public string ImapServer { get; set; }
         public int ImapPort { get; set; }
         public string ImapUsername { get; set; }
         public string ImapPassword { get; set; }
-        public EmailConfiguration()
+        public EmailConfiguration(string configPath)
         {
-            try
-            {
-                XElement conn = XElement.Load("connection.config");
-                ImapServer = (string)conn.Element("Server");
-                ImapPort = (int)conn.Element("Port");
-                ImapUsername = (string)conn.Element("UserName");
-                ImapPassword = (string)conn.Element("Password");
-                if (ImapServer == null || ImapPort == 0)
-                    throw new IOException("Connection file could not be found or have some empty values.");
-            }
-            catch (Exception ex)
-            {
-                System.Console.WriteLine(ex.Message);
-            }
-        }
-    }
-    public class EmailIDsExtractor
-    {
-        private EmailConfiguration emailConfiguration = new EmailConfiguration();
-        private Dictionary<string, EmailAddress> emailIDs;
-        public string outputFolder = "output";
-        public string fileName = "Email IDs";
-        public string fileExtension = ".csv";
-        public string tempFilePath = "temp.tmp";
-        private int lastEmailRead = 0;
-        private int filePartNo = 0;
-        private int tempCount = 0;
-        private void SetTempFileData(int lastEmailRead, int filePartNo, int tempCount)
-        {
-            try
-            {
-                using (StreamWriter sw = new StreamWriter(tempFilePath))
-                {
-                    sw.WriteLine(lastEmailRead);
-                    sw.WriteLine(filePartNo);
-                    this.tempCount += tempCount;
-                    sw.WriteLine(this.tempCount);
-                }
-            }
-            catch (IOException ex)
-            {
-                throw ex;
-            }
-        }
-        private void RestoreTempFileData()
-        {
-            try
-            {
-                if (File.Exists(tempFilePath))
-                    using (StreamReader sr = new StreamReader(tempFilePath))
-                    {
-                        lastEmailRead = int.Parse(sr.ReadLine());
-                        filePartNo = int.Parse(sr.ReadLine());
-                        tempCount = int.Parse(sr.ReadLine());
-                        DataExistedInTemp = true;
-                    }
-                
-            }
-            catch
-            {
-                //
-            }
-        }
-        private void CheckTempOutputFile()
-        {
-            System.Console.WriteLine("Output will be overwritten if files exists in output folder.");
-            System.Console.WriteLine("\nReading addresses...");
-            if (!DataExistedInTemp && File.Exists(Path.Combine(outputFolder, "temp output") + fileExtension))
-                File.Delete(Path.Combine(outputFolder, "temp output") + fileExtension);
-        }
-        public List<EmailAddress> GetEmailAddresses()
-        {
-            try
-            {
-                using (var client = new ImapClient())
-                {
-                    client.Connect(emailConfiguration.ImapServer, emailConfiguration.ImapPort, SecureSocketOptions.SslOnConnect);
+            XElement conn = XElement.Load(configPath);
 
-                    client.Authenticate(emailConfiguration.ImapUsername, emailConfiguration.ImapPassword);
+            ImapServer = conn.Element("Server")?.Value;
+            if (string.IsNullOrEmpty(ImapServer))
+                throw new Exception("Server must be specified.");
 
-                    client.Inbox.Open(FolderAccess.ReadOnly);
-                    emailIDs = new Dictionary<string, EmailAddress>();
-                    List<EmailAddress> tempEmailIDs = new List<EmailAddress>();
+            if (int.TryParse(conn.Element("Port")?.Value, out int port))
+                ImapPort = port;
+            else
+                throw new Exception("Port must be specified.");
 
-                    var inbox = client.Inbox;
-                    System.Console.WriteLine("Fetching Emails...");
-                    System.Console.WriteLine(inbox.Count + " emails found in Inbox.\nTemporary output files can be found in output\\temp folder.");
-                    var uids = client.Inbox.Search(SearchQuery.All);
+            ImapUsername = conn.Element("UserName")?.Value;
+            if (string.IsNullOrEmpty(ImapUsername))
+                throw new Exception("UserName must be specified.");
 
-                    RestoreTempFileData();
-                    CheckTempOutputFile();
-
-                    for (int i = lastEmailRead; i < uids.Count; i++)
-                    {
-                        var headers = inbox.GetHeaders(uids[i]);
-                        var addresses = headers["From"] + "," + headers["To"] + "," + headers["CC"];
-                        var allAddresses = addresses.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-
-                        foreach (var address in allAddresses)
-                        {
-                            var add = address.Split('<');
-                            string addr = (add.Count() > 1) ? add[1].TrimEnd('>') : add[0];
-                            string name = (add.Count() > 1) ? add[0] : "";
-                            if (!emailIDs.ContainsKey(addr))
-                            {
-                                var email = new EmailAddress();
-                                email.Address = addr;
-                                email.Name = name;
-                                tempEmailIDs.Add(email);
-                                emailIDs.Add(addr, email);
-                            }
-                        }
-                        if (tempEmailIDs.Count > 100)
-                        {
-                            tempEmailIDs.RemoveAll(x => x.Address == emailConfiguration.ImapUsername);
-                            WriteToFile(tempEmailIDs, "temp output");
-                            WriteToFile(tempEmailIDs, filePartNo);
-                            System.Console.WriteLine($"Extracted {tempEmailIDs.Count} emails.");
-							filePartNo++;
-                            SetTempFileData((i+1), filePartNo, tempEmailIDs.Count);                            
-                            tempEmailIDs.Clear();
-                        }
-                    }
-                    client.Disconnect(true);
-                    if (tempEmailIDs.Any())
-                    {
-                        WriteToFile(tempEmailIDs, "temp output");
-                        WriteToFile(tempEmailIDs, filePartNo);
-                        System.Console.WriteLine("All emails read from Inbox.");
-                    }
-                    RestoreExtractedEmails();
-                    return emailIDs.Values.ToList();
-                }
-            }
-            catch (AuthenticationException ex)
-            {
-                System.Console.WriteLine(ex.Message);
-                System.Console.WriteLine("Please check the configuration details in the config file.");
-            }
-            catch (System.Net.Sockets.SocketException ex)
-            {
-                System.Console.WriteLine("Could not connect to the email server!");
-                System.Console.WriteLine("Possible reasons:\n Network issue.\n Incorrect configuration details.");
-                System.Console.WriteLine(ex.Message);
-            }
-            return null;
-
-        }
-
-        public void WriteToFile(List<EmailAddress> emailAddresses, int fileNo)
-        {
-            try
-            {
-                string tempPath = Path.Combine(outputFolder, "temp");
-                if (!Directory.Exists(tempPath))
-                    Directory.CreateDirectory(tempPath);
-                using (StreamWriter sw = new StreamWriter(Path.Combine(tempPath, fileName) + fileNo + fileExtension))
-                {
-                    foreach (var email in emailAddresses)
-                    {
-                        sw.WriteLine(email.Name + "," + email.Address);
-                    }
-                }
-            }
-            catch (IOException ex)
-            {
-                System.Console.WriteLine(ex.Message);
-            }
-        }
-        public void WriteToFile(List<EmailAddress> emailAddresses, string fileName)
-        {
-            try
-            {
-                string tempOutputPath = Path.Combine(outputFolder, fileName) + fileExtension;
-                
-                if (!Directory.Exists(outputFolder))
-                    Directory.CreateDirectory(outputFolder);
-                using (StreamWriter sw = new StreamWriter(tempOutputPath, true))
-                {
-                    foreach (var email in emailAddresses)
-                    {
-                        sw.WriteLine(email.Name + "," + email.Address);
-                    }
-                }
-            }
-            catch (IOException ex)
-            {
-                System.Console.WriteLine(ex.Message);
-            }
-        }
-
-        private bool DataExistedInTemp = false;
-        void RestoreExtractedEmails()
-        {
-            try
-            {
-                if (DataExistedInTemp)
-                {
-                    System.Console.WriteLine("Temporary output file was found. Merging previously read data...");
-                    if (File.Exists(Path.Combine(outputFolder, "temp output") + fileExtension))
-                        using (StreamReader sr = new StreamReader(Path.Combine(outputFolder, "temp output") + fileExtension))
-                        {
-                            int i = 0;
-                            string line;
-                            while ((line = sr.ReadLine()) != null)
-                            {
-                                var email = new EmailAddress();
-                                email.Name = line.Split(',')[0];
-                                email.Address = line.Split(',')[1];
-                                emailIDs.Add(email.Address, email);
-                                i++;
-                                if (i > tempCount)
-                                    break;
-                            }
-                        }
-                }
-
-            }
-            catch
-            {
-                //
-            }
+            ImapPassword = conn.Element("Password")?.Value;
+            if (string.IsNullOrEmpty(ImapPassword))
+                throw new Exception("Password must be specified.");
         }
     }
     class Program
     {
-        static EmailIDsExtractor extractor = new EmailIDsExtractor();
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Program started.");
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+
+            EmailConfiguration emailConfiguration;
             try
             {
-                var emailAddresses = extractor.GetEmailAddresses();
-                if (emailAddresses.Any())
+                emailConfiguration = new EmailConfiguration(Path.Combine(path, "connection.config"));
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("connection.config file could not be loaded. " + ex.Message);
+                return;
+            }
+
+            string outputsPath = Path.Combine(path, "Outputs");
+            Directory.CreateDirectory(outputsPath);
+            int readCount = GetEmailCount(outputsPath);
+            if (readCount > 0)
+            {
+                Console.WriteLine($"{readCount} emails were already processed earlier. These will be skipped");
+            }
+
+            ImapClient client = null;
+            try
+            {
+                while (true)
                 {
-                    extractor.WriteToFile(emailAddresses, extractor.fileName);
-                    System.Console.WriteLine($"\n Output File: {Path.GetFullPath(Path.Combine(extractor.outputFolder, extractor.fileName) + extractor.fileExtension)}");
-					File.Delete(Path.Combine(extractor.outputFolder + "temp output" + extractor.fileExtension));
-                    File.Delete(extractor.tempFilePath);
-                }
-                else
-                {
-                    System.Console.WriteLine("No emails found!");
+                    int prevCount = readCount;
+                    string contactsPath = MakeFilePath(outputsPath, readCount);
+                    var list = ProcessBatch(ref client, () => CreateClient(emailConfiguration), ref readCount);
+                    if (prevCount == readCount)
+                    {
+                        list = ReadAddresses(contactsPath);
+                        string backup = contactsPath + ".backup";
+                        File.Move(contactsPath, backup);
+                        WriteAddresses(list, contactsPath);
+                        File.Delete(backup);
+                        Console.WriteLine("Entire inbox processed");
+                        Console.WriteLine("Press any key to close.");
+                        Console.ReadKey();
+                        return;
+                    }
+                    else
+                    {
+                        WriteAddresses(list, contactsPath);
+                        File.Move(contactsPath, MakeFilePath(outputsPath, readCount));
+                        Console.WriteLine($"{readCount} emails processed");
+                    }
                 }
             }
             catch (Exception ex)
             {
-                System.Console.WriteLine(ex.Message);
+                Console.Error.WriteLine(ex.Message);
+                Console.WriteLine("You can try running the program again. It will continue from the point of failure.");
+                Console.WriteLine("Press any key to close.");
+                Console.ReadKey();
             }
-            System.Console.WriteLine("\nPress any key to close.");
-            Console.ReadKey();
+            finally
+            {
+                if (client != null) client.Dispose();
+            }
+        }
+
+        const string FilePrefix = "contacts-";
+        static string MakeFilePath(string folder, int count)
+        {
+            return Path.Combine(folder, $"{FilePrefix}{count:0}.csv");
+        }
+        static int GetEmailCount(string folderPath)
+        {
+            foreach (var path in Directory.GetFiles(folderPath, $"{FilePrefix}*.csv"))
+            {
+                if (int.TryParse(Path.GetFileNameWithoutExtension(path).Substring(FilePrefix.Length), out int count))
+                    return count;
+            }
+            return 0;
+        }
+
+        static ImapClient CreateClient(EmailConfiguration configuration)
+        {
+            var client = new ImapClient();
+            client.Connect(configuration.ImapServer, configuration.ImapPort, SecureSocketOptions.SslOnConnect);
+            client.Authenticate(configuration.ImapUsername, configuration.ImapPassword);
+            client.Inbox.Open(FolderAccess.ReadOnly);
+            return client;
+        }
+
+        static HashSet<MailboxAddress> ProcessBatch(ref ImapClient client, Func<ImapClient> create, ref int readCount)
+        {
+            int retries = 3;
+            for (int i = 0; i < retries; i++)
+            {
+                try
+                {
+                    if (client == null)
+                        client = create();
+
+                    var list = new HashSet<MailboxAddress>(new AddressComparer());
+                    var messages = client.Inbox.Fetch(readCount, readCount + 199, MessageSummaryItems.Headers);
+                    int lastIndex = -1;
+                    foreach (var msg in messages)
+                    {
+                        lastIndex = Math.Max(lastIndex, msg.Index);
+                        ParseInto(msg.Headers["From"] ?? "", list);
+                        ParseInto(msg.Headers["To"] ?? "", list);
+                        ParseInto(msg.Headers["CC"] ?? "", list);
+                    }
+                    if (lastIndex >= 0)
+                        readCount = lastIndex + 1;
+                    return list;
+                }
+                catch when (i < retries - 1)
+                {
+                    if (client != null)
+                        client.Dispose();
+                }
+            }
+            return null;//This is not actually reachable
+        }
+        private static void ParseInto(string value, HashSet<MailboxAddress> list)
+        {
+            if (InternetAddressList.TryParse(value, out var addresses))
+            {
+                foreach (var address in addresses)
+                {
+                    if (address is MailboxAddress a)
+                    {
+                        list.Add(a);
+                    }
+                }
+            }
+        }
+
+        private static void WriteAddresses(IEnumerable<MailboxAddress> addresses, string path)
+        {
+            if (!File.Exists(path))
+                File.WriteAllText(path, $"Name,Email{Environment.NewLine}");
+
+            File.AppendAllLines(path, addresses.Select(a => $"{Escape(a.Name)},{Escape(a.Address)}"));
+        }
+
+        private static string Escape(string s)
+        {
+            return "\"" + s.Replace("\"", "\"\"") + "\"";
+        }
+
+        private static HashSet<MailboxAddress> ReadAddresses(string path)
+        {
+            var list = new HashSet<MailboxAddress>(new AddressComparer());
+            var q = "\"";
+            var regex = new Regex($@"{q}((?:[^{q}]|{q}{q})*){q},{q}((?:[^{q}]|{q}{q})*){q}");
+            foreach (var line in File.ReadLines(path).Skip(1))
+            {
+                var match = regex.Match(line);
+                var name = match.Groups[1].Value.Replace("\"\"", "\"");
+                var address = match.Groups[2].Value.Replace("\"\"", "\"");
+                list.Add(new MailboxAddress(name, address));
+            }
+            return list;
+        }
+
+        class AddressComparer : IEqualityComparer<MailboxAddress>
+        {
+            private static readonly StringComparer sc = StringComparer.OrdinalIgnoreCase;
+            public bool Equals(MailboxAddress x, MailboxAddress y)
+            {
+                return sc.Equals(x.Name, y.Name) && sc.Equals(x.Address, y.Address);
+            }
+
+            public int GetHashCode(MailboxAddress obj)
+            {
+                return HashCode.Combine(sc.GetHashCode(obj.Name), sc.GetHashCode(obj.Address));
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Email-ID-Extractor
 This tool scans the Inbox of the provided email account and extracts all the email addresses into a CSV file.
 Note:
-1) Email credentials need to be provided in the connection.config file.
-2) The temporary output will be provided in the output\temp folder.
-3) If the program crashes in between execution, the program picks up from where it left off and at the end merges the email_ids from the temp file.
-4) The final output file with all unique email ids will be generated at the end of the program execution.
+1) Copy the connection.config.sample file to connection.config and enter your credentials in this new file. This file is ignored by source control to protect leakage of credentials.
+2) The output file is located in the Outputs folder next to the compiled exe.
+3) The program picks up from where it left off (even in case of an error or crash) by storing a count of emails processed as part of the output file name and updating the output file periodically. By keeping this file around, you can run the tool again at any time and it will only process the new emails since the last time it was run.
+4) When the program ends successfully, it removes duplicates from the output file.
